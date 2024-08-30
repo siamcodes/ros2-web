c66a5720600108bde3d90ae8cb4ce60920d8b0e0
siamcodes <siamcodes@gmail.com>
2024-08-30 14:42:50 +0700

    uodate

---
+++ src/components/Connection.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Config from './config/config'
+
+export default class Connection extends Component {
+  render() {
+    return (
+      <div>Connection</div>
+    )
+  }
+}
